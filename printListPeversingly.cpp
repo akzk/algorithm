acf5d7d527ade559860eb783d8894b80243124ae
akzk <leonardocaesarz@gmail.com>
2018-01-09 03:13:10 +0800

    finish printListPeversingly

---
+++ printListPeversingly.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct ListNode
+{
+    int num;
+    ListNode* next;
+};
+
+void printListPerversingly1(ListNode* node)
+{
+    if (node != NULL) {
+        printListPerversingly1(node->next);
+        printf("%d ", node->num);
+    }
+}
+
+void printListPerversingly2(ListNode* head)
+{
+    std::stack<ListNode*> list;
+
+    ListNode* node = head;
+    while (node != nullptr)
+    {
+        list.push(node);
+        node = node->next;
+    }
+
+    while (!list.empty())
+    {
+        node = list.top();
+        printf("%d ", node->num);
+        list.pop();
+    }
+}
+
+void show(ListNode* node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->num);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    ListNode* head = (ListNode*)malloc(sizeof(ListNode));
+    head->num = 0;
+    head->next = NULL;
+
+    ListNode* cur = head;
+    ListNode* tmp = NULL;
+
+    int nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i=0; i<10; i++)
+    {
+        tmp = (ListNode*)malloc(sizeof(ListNode));
+        tmp->num = nums[i];
+        tmp->next = NULL;
+
+        cur->next = tmp;
+        cur = tmp;
+    }
+
+    show(head);
+    printListPerversingly1(head);
+    printListPerversingly2(head);
+}
